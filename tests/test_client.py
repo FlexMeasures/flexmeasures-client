295dd15856ef5bb113faeece2b9c8115c9afb5be
victor <victor@seita.nl>
2023-04-20 17:05:40 +0200

    Testing aioresponses mock.
    Signed-off-by: victor <victor@seita.nl>

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,43 +1,21 @@
-import json
-from unittest.mock import AsyncMock, MagicMock
-
-import aiohttp
 import pytest
-import pytest_asyncio
-import pytest_mock
-from aiohttp import ClientSession
+from aioresponses import aioresponses
 
 from flexmeasures_client.client import FlexmeasuresClient
 
-flexmeasures_client = FlexmeasuresClient("test", "test")
-
-
-class MockResponse:
-    def __init__(self, text, status):
-        self._text = text
-        self.status = status
-
-    async def text(self):
-        return self._text
-
-    async def __aexit__(self, exc_type, exc, tb):
-        pass
-
-    async def __aenter__(self):
-        return self
-
 
 @pytest.mark.asyncio
-async def test_get_access_token(mocker):
-    mock = aiohttp.ClientSession
-    mock.request = AsyncMock()
-    mock.__aenter__.status = 200
-    mock.__aenter__.headers.get = ["application/json"]
-    mock.__aenter__.data = {"test": "test"}
-    data = {}
+async def test_get_access_token() -> None:
+    with aioresponses() as m:
+        m.post(
+            "http://localhost:5000/api/requestAuthToken",
+            status=200,
+            payload={"auth_token": "test-token"},
+        )
+        flexmeasures_client = FlexmeasuresClient("test", "test")
 
-    resp_dict = await flexmeasures_client.get_access_token()
-
-
-def test_fail():
-    assert 1 == 2
+        resp = (
+            await flexmeasures_client.get_access_token()
+        )  # await session.get('http://example.com')
+        print(resp)
+        assert resp == "test-token"
