51dae0bca7ad594d5b06d08acbb0dcf6cc6776c0
Victor Garcia Reolid <victor@seita.nl>
2023-05-26 12:08:33 +0200

    test: add assert and cases
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>

--- tests/test_frbc_utils.py
+++ tests/test_frbc_utils.py
@@ -1,14 +1,36 @@
+import math
+
 import pytest
 from python_s2_protocol.common.schemas import CommodityQuantity, NumberRange, PowerRange
 from python_s2_protocol.FRBC.schemas import FRBCOperationMode, FRBCOperationModeElement
 
-from flexmeasures_client.s2.control_types.FRBC.utils import compute_factor
+from flexmeasures_client.s2.control_types.FRBC.utils import (
+    compute_factor,
+    get_unique_id,
+)
 
 
-@pytest.mark.parametrize("fill_level, fill_rate, factor, fails", [(0.1, 0.3, 1, False)])
-def test_compute_factor(
-    fill_level: float, fill_rate: float, factor: float, fails: bool
-):
+@pytest.mark.parametrize(
+    "fill_level, fill_rate, expected_factor",
+    [
+        (-1, 0.1, None),
+        (10, 0.1, None),
+        (0.1, 0.1, 0.1),
+        (0.26, 0.1, 0.05),
+        (0.75, 0.1, 0.05),
+    ],
+)
+def test_compute_factor(fill_level: float, fill_rate: float, expected_factor: float):
+    """_summary_
+
+    :param fill_level: _description_
+    :type fill_level: float
+    :param fill_rate: _description_
+    :type fill_rate: float
+    :param expected_factor: _description_
+    :type expected_factor: float
+    """
+
     fill_rate_start = 0
 
     power_range = PowerRange(
@@ -19,15 +41,12 @@ def test_compute_factor(
 
     operation_mode_elements = []
 
-    for level in range(10):
-        # constant fill_level ranges of length 10
-        fill_level_start = level * 10
-        fill_level_end = fill_level_start + 10
+    fill_level_range = [(0, 0.25), (0.25, 0.75), (0.75, 1)]
+    fill_rate_range = [(0, 1), (0, 2), (0, 1)]
 
-        # slope of line will be `level`, 10, 20, 30, ..
-        fill_rate_start = 0
-        fill_rate_end = (level + 1) * 10
-
+    for (fill_level_start, fill_level_end), (fill_rate_start, fill_rate_end) in zip(
+        fill_level_range, fill_rate_range
+    ):
         operation_mode_element = FRBCOperationModeElement(
             fill_level_range=NumberRange(
                 start_of_range=fill_level_start, end_of_range=fill_level_end
@@ -42,10 +61,15 @@ def test_compute_factor(
         operation_mode_elements.append(operation_mode_element)
 
     operation_mode = FRBCOperationMode(
-        id="10",
+        id=get_unique_id(),
         elements=operation_mode_elements,
         abnormal_condition_only=False,
         diagnostic_label="",
     )
 
-    compute_factor(operation_mode, 0.1, 10)
+    factor = compute_factor(operation_mode, fill_level, fill_rate)
+
+    if expected_factor:
+        assert math.isclose(factor, expected_factor)
+    else:
+        assert factor is None
