a0a65c679804ab2eb96e2e518eae2aeb79c833f7
F.N. Claessen <felix@seita.nl>
2023-05-26 14:00:12 +0200

    Allow top-level import of FlexMeasuresClient
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- src/flexmeasures_client/__init__.py
+++ src/flexmeasures_client/__init__.py
@@ -1,5 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
+from client import FlexMeasuresClient
+
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = "flexmeasures-client"
