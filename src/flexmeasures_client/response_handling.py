ca6879cfa9c19c524c35cbed3aa32fcb5bed5905
F.N. Claessen <felix@seita.nl>
2023-12-07 13:54:24 +0100

    refactor: handle response status codes in a more intuitive order
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- src/flexmeasures_client/response_handling.py
+++ src/flexmeasures_client/response_handling.py
@@ -18,8 +18,9 @@ async def check_response(
     """
     <300: passes
     303: redirect to new url
+    400 + custom message: schedule not ready yet
     401: reauthenticate
-    todo: 503 + Retry-After header: poll again
+    503 + Retry-After header: poll again
     otherwise: call error_handler
     """
     status = response.status
@@ -31,6 +32,15 @@ async def check_response(
         message = f"Redirect to fallback schedule: {response.headers['location']}"  # noqa: E501
         logging.debug(message)
         url = response.headers["location"]
+    elif status == 400 and (
+        "Scheduling job waiting" in payload.get("message", "")
+        or "Scheduling job in progress" in payload.get("message", "")
+    ):
+        # can be removed in a later version GH issue #645 of the FlexMeasures repo
+        message = f"Server indicated to try again later. Retrying in {self.polling_interval} seconds..."  # noqa: E501
+        logging.debug(message)
+        polling_step += 1
+        await asyncio.sleep(self.polling_interval)
     elif status == 401 and reauth_once:
         message = f"""Authentication failed with"
         status: {status}
@@ -42,15 +52,6 @@ async def check_response(
         await self.get_access_token()
         reauth_once = False
     elif status == 503 and "Retry-After" in headers:
-        polling_step += 1
-        await asyncio.sleep(self.polling_interval)
-    elif status == 400 and (
-        "Scheduling job waiting" in payload.get("message", "")
-        or "Scheduling job in progress" in payload.get("message", "")
-    ):
-        # can be removed in a later version GH issue #645 of the FlexMeasures repo
-        message = f"Server indicated to try again later. Retrying in {self.polling_interval} seconds..."  # noqa: E501
-        logging.debug(message)
         polling_step += 1
         await asyncio.sleep(self.polling_interval)
     elif payload.get("errors"):
