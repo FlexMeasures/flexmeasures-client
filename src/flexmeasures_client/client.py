fda51feac3e02a982d74b6739a18c02a7584cc89
Victor Garcia Reolid <victor@seita.nl>
2024-08-07 16:56:51 +0200

    make session optional
    Signed-off-by: Victor Garcia Reolid <victor@seita.nl>
    Signed-off-by: Vlad Iftime <vladiftime60@gmail.com>

--- src/flexmeasures_client/client.py
+++ src/flexmeasures_client/client.py
@@ -7,7 +7,7 @@ import re
 import socket
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from typing import Any
+from typing import Any, cast
 
 import async_timeout
 import pandas as pd
@@ -59,7 +59,8 @@ class FlexMeasuresClient:
     session: ClientSession | None = None
 
     def __post_init__(self):
-        self.session: ClientSession = ClientSession()
+        if self.session is None:
+            self.session = ClientSession()
 
         if not re.match(r".+\@.+\..+", self.email):
             raise EmailValidationError(
@@ -104,7 +105,7 @@ class FlexMeasuresClient:
 
     async def close(self):
         """Function to close FlexMeasuresClient session when all requests are done"""
-        await self.session.close()
+        await cast(ClientSession, self.session).close()
 
     async def request(
         self,
@@ -199,8 +200,12 @@ class FlexMeasuresClient:
         logging.debug("=" * 14)
 
         """Sends a single request to FlexMeasures and checks the response"""
+<<<<<<< HEAD
         self.ensure_session()
         response = await self.session.request(  # type: ignore
+=======
+        response = await cast(ClientSession, self.session).request(
+>>>>>>> 3b3ca0b (make session optional)
             method=method,
             url=url,
             params=params,
