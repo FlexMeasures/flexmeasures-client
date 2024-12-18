23fefbc94901ae03ee88550b8fac582b6b55a17f
Vlad Iftime <vlad.iftime@solarracinggroningen.nl>
2024-12-19 00:11:43 +0200

    Created tests for PPBC. Passed all the tests. One test left.

--- tests/test_cem.py
+++ tests/test_cem.py
@@ -40,11 +40,12 @@ async def test_handshake(rm_handshake):
 
 
 @pytest.mark.asyncio
-async def test_resource_manager_details(resource_manager_details, rm_handshake):
+async def test_resource_manager_details(resource_manager_details_frbc, rm_handshake):
     cem = CEM(fm_client=None)
     frbc = FRBCTest()
-
+    ppbc = PPBC()
     cem.register_control_type(frbc)
+    cem.register_control_type(ppbc)
 
     #############
     # Handshake #
@@ -63,7 +64,7 @@ async def test_resource_manager_details(resource_manager_details, rm_handshake):
     ##########################
 
     # RM sends ResourceManagerDetails
-    await cem.handle_message(resource_manager_details)
+    await cem.handle_message(resource_manager_details_frbc)
     response = await cem.get_message()
 
     # CEM response is ReceptionStatus with an OK status
@@ -71,7 +72,7 @@ async def test_resource_manager_details(resource_manager_details, rm_handshake):
     assert response["status"] == "OK"
 
     assert (
-        cem._resource_manager_details == resource_manager_details
+        cem._resource_manager_details == resource_manager_details_frbc
     ), "CEM should store the resource_manager_details"
     assert cem.control_type == ControlType.NO_SELECTION, (
         "CEM control type should switch to ControlType.NO_SELECTION,"
@@ -80,8 +81,8 @@ async def test_resource_manager_details(resource_manager_details, rm_handshake):
 
 
 @pytest.mark.asyncio
-async def test_activate_control_type(
-    frbc_system_description, resource_manager_details, rm_handshake
+async def test_activate_control_type_frbc(
+    frbc_system_description, resource_manager_details_frbc, rm_handshake
 ):
     cem = CEM(fm_client=None)
     frbc = FRBCTest()
@@ -98,7 +99,7 @@ async def test_activate_control_type(
     ##########################
     # ResourceManagerDetails #
     ##########################
-    await cem.handle_message(resource_manager_details)
+    await cem.handle_message(resource_manager_details_frbc)
     response = await cem.get_message()
 
     #########################
@@ -126,8 +127,54 @@ async def test_activate_control_type(
 
 
 @pytest.mark.asyncio
-async def test_messages_route_to_control_type_handler(
-    frbc_system_description, resource_manager_details, rm_handshake
+async def test_activate_control_type_ppbc(
+    ppbc_power_profile_definition, resource_manager_details_ppbc, rm_handshake
+):
+    cem = CEM(fm_client=None)
+    ppbc = PPBC()
+
+    cem.register_control_type(ppbc)
+
+    #############
+    # Handshake #
+    #############
+
+    await cem.handle_message(rm_handshake)
+    response = await cem.get_message()
+
+    ##########################
+    # ResourceManagerDetails #
+    ##########################
+    await cem.handle_message(resource_manager_details_ppbc)
+    response = await cem.get_message()
+
+    #########################
+    # Activate control type #
+    #########################
+
+    # CEM sends a request to change te control type
+    await cem.activate_control_type(ControlType.POWER_PROFILE_BASED_CONTROL)
+    message = await cem.get_message()
+
+    assert cem.control_type == ControlType.NO_SELECTION, (
+        "the control type should still be NO_SELECTION (rather than PPBC),"
+        " because the RM has not yet confirmed PPBC activation"
+    )
+
+    response = ReceptionStatus(
+        subject_message_id=message.get("message_id"), status=ReceptionStatusValues.OK
+    )
+
+    await cem.handle_message(response)
+
+    assert (
+        cem.control_type == ControlType.POWER_PROFILE_BASED_CONTROL
+    ), "after a positive ResponseStatus, the status changes from NO_SELECTION to PPBC"
+
+
+@pytest.mark.asyncio
+async def test_messages_route_to_control_type_handler_frbc(
+    frbc_system_description, resource_manager_details_frbc, rm_handshake
 ):
     cem = CEM(fm_client=None)
     frbc = FRBCTest()
@@ -144,7 +191,7 @@ async def test_messages_route_to_control_type_handler(
     ##########################
     # ResourceManagerDetails #
     ##########################
-    await cem.handle_message(resource_manager_details)
+    await cem.handle_message(resource_manager_details_frbc)
     response = await cem.get_message()
 
     #########################
@@ -204,133 +251,6 @@ async def test_messages_route_to_control_type_handler(
     ), "success callback should be deleted"
 
 
+# TODO: Add the following tests
 # @pytest.mark.asyncio
-# async def test_cem_ppbc():  # TODO: move into different test functions
-#     cem = CEM(sensor_id=1, fm_client=None)
-#     ppbc = PPBC()
-#     cem.register_control_type(ppbc)
-#     # Show that this point is reached
-
-#     #############
-#     # Handshake #
-#     #############
-
-#     handshake_message = Handshake(
-#         message_id=get_unique_id(),
-#         role=EnergyManagementRole.RM,
-#         supported_protocol_versions=["0.1.0"],
-#     )
-
-#     await cem.handle_message(handshake_message)
-
-#     assert (
-#         cem._sending_queue.qsize() == 1
-#     )  # check that message is put to the outgoing queue
-
-#     response = await cem.get_message()
-
-#     assert (
-#         response["message_type"] == "HandshakeResponse"
-#     ), "response message_type should be HandshakeResponse"
-#     assert (
-#         response["selected_protocol_version"] == "0.1.0"
-#     ), "CEM selected protocol version should be supported by the Resource Manager"
-
-#     ##########################
-#     # ResourceManagerDetails #
-#     ##########################
-
-#     resource_manager_details_message = ResourceManagerDetails(
-#         message_id=get_unique_id(),
-#         resource_id=get_unique_id(),
-#         roles=[Role(role=RoleType.ENERGY_CONSUMER, commodity=Commodity.ELECTRICITY)],
-#         instruction_processing_delay=Duration(1),
-#         available_control_types=[
-#             ControlType.FILL_RATE_BASED_CONTROL,
-#             ControlType.POWER_PROFILE_BASED_CONTROL,
-#             ControlType.NO_SELECTION,
-#         ],
-#         provides_forecast=True,
-#         provides_power_measurement_types=[
-#             CommodityQuantity.ELECTRIC_POWER_3_PHASE_SYMMETRIC
-#         ],
-#     )
-
-#     await cem.handle_message(resource_manager_details_message)
-#     response = await cem.get_message()
-
-#     assert response["message_type"] == "ReceptionStatus"
-#     assert response["status"] == "OK"
-#     assert (
-#         cem._resource_manager_details == resource_manager_details_message
-#     ), "CEM should store the resource_manager_details"
-#     assert cem.control_type == ControlType.NO_SELECTION, (
-#         "CEM control type should switch to ControlType.NO_SELECTION,"
-#         "independently of the original type"
-#     )
-
-#     #########################
-#     # Activate control type #
-#     #########################
-
-#     await cem.activate_control_type(ControlType.POWER_PROFILE_BASED_CONTROL)
-#     message = await cem.get_message()
-
-#     assert cem.control_type == ControlType.NO_SELECTION, (
-#         "the control type should still be NO_SELECTION (rather than PPBC),"
-#         " because the RM has not yet confirmed FRBC activation"
-#     )
-
-#     response = ReceptionStatus(
-#         subject_message_id=message.get("message_id"), status=ReceptionStatusValues.OK
-#     )
-
-#     await cem.handle_message(response)
-
-#     assert (
-#         cem.control_type == ControlType.POWER_PROFILE_BASED_CONTROL
-#     ), "after a positive ResponseStatus, the status changes from NO_SELECTION to FRBC"
-
-#     ########
-#     # PPBC #
-#     ########
-
-
-#     await cem.handle_message(system_description_message)
-#     response = await cem.get_message()
-
-#     # checking that FRBC handler is being called
-#     assert (
-#         cem._control_types_handlers[
-#             ControlType.POWER_PROFILE_BASED_CONTROL
-#         ]._system_description_history[str(system_description_message.message_id)]
-#         == system_description_message
-#     ), (
-#         "the FRBC.SystemDescription message should be stored"
-#         "in the frbc.system_description_history variable"
-#     )
-
-#     # change of control type is not performed in case that the RM answers
-#     # with a negative response
-#     await cem.activate_control_type(ControlType.NO_SELECTION)
-#     response = await cem.get_message()
-#     assert (
-#         cem._control_type == ControlType.FILL_RATE_BASED_CONTROL
-#     ), "control type should not change, confirmation still pending"
-
-#     await cem.handle_message(
-#         ReceptionStatus(
-#             subject_message_id=response.get("message_id"),
-#             status=ReceptionStatusValues.INVALID_CONTENT,
-#         )
-#     )
-
-#     assert (
-#         cem._control_type == ControlType.FILL_RATE_BASED_CONTROL
-#     ), "control type should not change, confirmation state is not 'OK'"
-#     assert (
-#         response.get("message_id")
-#         not in cem._control_types_handlers[
-#             ControlType.FILL_RATE_BASED_CONTROL
-#         ].success_callbacks
-#     ), "success callback should be deleted"
+# async def test_messages_route_to_control_type_handler_ppbc(
