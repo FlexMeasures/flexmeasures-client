36d145c3065b38b8197bc584a5c69904b5184b3e
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2025-09-11 17:30:39 +0200

    feat: add --max-forecast-horizon argument to PV forecast command. set to FORECAST_HORIZON_HOURS equal to 24hs
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- examples/HEMS_setup.py
+++ examples/HEMS_setup.py
@@ -543,6 +543,8 @@ async def generate_pv_forecasts(client: FlexMeasuresClient):
         "2025-01-08T00:00:00+00:00",
         "--to-date",
         "2025-01-21T23:59:59+00:00",
+        "--max-forecast-horizon",
+        f"PT{FORECAST_HORIZON_HOURS}H",
     ]
 
     result = subprocess.run(cmd, capture_output=True, text=True, timeout=300)
