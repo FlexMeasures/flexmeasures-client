420f758ed9c57b37c9f84dd0a08079caaf699ab0
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2025-09-30 02:49:20 +0200

    debug: comment out total_energy_costs reporter run as it breaks when running
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- examples/HEMS_setup.py
+++ examples/HEMS_setup.py
@@ -1838,14 +1838,15 @@ async def create_reporters(client: FlexMeasuresClient):
         end=SCHEDULING_END,
     )
 
-    # Run TotalEnergyCostsReporter command
-    total_energy_costs_result = run_reporter_cmd(
-        reporter_map={"name": "total-energy-costs", "reporter": "PandasReporter"},
-        start=SCHEDULING_START,
-        end=SCHEDULING_END,
-    )
+    ## Commented out for debugging purposes
+    # # Run TotalEnergyCostsReporter command
+    # total_energy_costs_result = run_reporter_cmd(
+    #     reporter_map={"name": "total-energy-costs", "reporter": "PandasReporter"},
+    #     start=SCHEDULING_START,
+    #     end=SCHEDULING_END,
+    # )
 
-    return self_consumption_result and aggregate_result and total_energy_costs_result
+    return self_consumption_result and aggregate_result #and total_energy_costs_result
 
 
 async def main():
