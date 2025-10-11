718967205a4975e087eb92e834932b5180ba7a50
F.N. Claessen <felix@seita.nl>
2025-10-11 15:15:23 +0200

    dev: log first, then take action
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- src/flexmeasures_client/s2/control_types/FRBC/frbc_tunes.py
+++ src/flexmeasures_client/s2/control_types/FRBC/frbc_tunes.py
@@ -146,11 +146,11 @@ class FillRateBasedControlTUNES(FRBC):
 
     def is_timer_due(self, name: str):
         if self._timers.get(name, datetime.now() - self._minimum_measurement_period) < datetime.now():
-            self._timers[name] = datetime.now() + self._minimum_measurement_period
             self._logger.debug(f"Timer for {name} is due!")
             self._logger.debug(self._timers.get(name, datetime.now()))
             self._logger.debug(self._timers.get(name, datetime.now() - self._minimum_measurement_period))
             self._logger.debug(datetime.now())
+            self._timers[name] = datetime.now() + self._minimum_measurement_period
             return True
         else:
             self._logger.debug(f"Timer for {name} is not due until {self._timers[name]}")
