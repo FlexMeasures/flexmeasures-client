b823583227452effc572cdb9c740d7d769a16c15
F.N. Claessen <felix@seita.nl>
2025-10-14 15:49:52 +0200

    Revert "feat: log timestamp converted to local timezone"
    This reverts commit 3d803918ef38e23f3f7534d95cc97d6b52039058.

--- src/flexmeasures_client/s2/control_types/FRBC/frbc_tunes.py
+++ src/flexmeasures_client/s2/control_types/FRBC/frbc_tunes.py
@@ -497,7 +497,7 @@ class FillRateBasedControlTUNES(FRBC):
         self._logger.debug("12")
 
         instructions = fm_schedule_to_instructions(
-            schedule, system_description, soc_at_start, logger=self._logger, timezone=self._timezone
+            schedule, system_description, soc_at_start, logger=self._logger
         )
 
         # Put the instruction in the sending queue
