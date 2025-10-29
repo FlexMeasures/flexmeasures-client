7d371b61c14ba5052ca50a9e65d286bc3b94742a
Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>
2025-10-29 12:47:33 +0100

    feat: add dynamic flex model for heating with current state of charge handling
    Signed-off-by: Mohamed Belhsan Hmida <mohamedbelhsanhmida@gmail.com>

--- examples/HEMS_setup.py
+++ examples/HEMS_setup.py
@@ -1557,6 +1557,16 @@ async def run_scheduling_simulation(
                     constraints=evse2_constraints,
                 )
 
+            if heating_next_current_soc is None:
+                # Use initial SoC for first step
+                heating_current_soc = heating_flex_model.get("soc_at_start", 12.0)
+            else:
+                heating_current_soc = heating_next_current_soc
+            # Create dynamic flex model for heating (Current SoC updated each step)
+            heating_scheduling_dynamic_flex_model = create_dynamic_storage_flex_model(
+                current_soc=heating_current_soc,
+            )
+
             # Start with the battery and PV flex models
             curtailable_pv_flex_model = {
                 "power-capacity": "12 kW",
