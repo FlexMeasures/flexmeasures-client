5da2ba384cbb0bd580d3568fa68631b12f0a06fa
F.N. Claessen <felix@seita.nl>
2025-11-12 15:07:55 +0100

    chore: mark off completed todos
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- examples/HEMS/HEMS_setup.py
+++ examples/HEMS/HEMS_setup.py
@@ -63,8 +63,6 @@ async def main(community_name: str, site_names: list[str]):
                 "Creating community Site asset with 2 building assets, each with PV and battery sensors, and weather station"
             )
             await create_community_asset(client, account, community_name=community_name, site_names=site_names)
-            # todo A1: create 2 sites and register them as children of a community asset (config variables become lists?)
-            # todo B1: the community asset should get a site-power-capacity sensor, and a flex-context with the site-power-capacity field referencing that sensor, and a power sensor
         else:
             answer = input(f"Asset '{community_name}' already exists. Re-create?")
             if answer.lower() in ["y", "yes"]:
@@ -76,27 +74,16 @@ async def main(community_name: str, site_names: list[str]):
         # Part 2: Upload data for first two weeks
         print("\n" + "=" * 50)
         print("PART 2: UPLOADING DATA")
-        # todo A2: upload data for the 2 sites as before
-        # todo B3: fill the site-power-capacity sensor with a CSV file with 30 kVA (note that the sum of the sites' max_consumption_capacity / site-power-capacity is 34.5 kW, 40 kVA, respectively)
         await upload_data_for_first_two_weeks(client, community_name=community_name, site_names=site_names)
 
         # Part 3: Generate PV forecasts for second week
         print("\n" + "=" * 50)
         print("PART 3: GENERATING PV FORECASTS")
-        # todo A3: forecast data for the 2 sites as before
         await generate_forecasts(client, site_names=site_names)
 
         # Part 4: Run scheduling simulation for third week
         print("\n" + "=" * 50)
         print("PART 4: SCHEDULING SIMULATION")
-        # todo A4: schedule each site separately
-        # todo A5: rerun tutorial and review
-        # todo B4: after each scheduling step, run a reporter to save the community's aggregate power schedule to the power sensor (maybe compute this as part of the community scheduler)
-        # todo B5: rerun tutorial and review: we now expect to see community capacity breaches
-        # todo C1: after the reporter from B4 is finished, determine the expected breaches (use some margin setting to decide between expecting a breach or not expecting a breach; we can refine this later)
-        # todo C2: if there are expected breaches, increase the site-peak-consumption-price in the flex-contexts of both sites within the period of the breach (the margin setting and the price delta should be defined in the same policy variable, e.g. `{"30 kW": 1 EUR/MWh", "5 kW": "100 EUR/MWh", "1 kW": "1000 EUR/MWh"}`)
-        # todo C3: rerun tutorial and review: we now expect to see a change in community capacity breaches
-        # todo C4: experiment with the community policy
         await run_scheduling_simulation(client, community_name=community_name, site_names=site_names)
 
         # Part 5 : Create reports
