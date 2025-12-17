faa1dcb5b6a65e23a3b9f73cc7b911f2ee900b68
F.N. Claessen <felix@seita.nl>
2025-12-17 09:30:57 +0100

    fix: convert values to list of floats instead of to np.ndarray
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- src/flexmeasures_client/s2/control_types/FRBC/frbc_tunes.py
+++ src/flexmeasures_client/s2/control_types/FRBC/frbc_tunes.py
@@ -735,7 +735,7 @@ class FillRateBasedControlTUNES(FRBC):
             await self._fm_client.post_sensor_data(
                 sensor_id=self._soc_minima_sensor_id,
                 start=fill_level_target_profile.start_time,
-                values=soc_minima.values,
+                values=soc_minima.tolist(),
                 unit=POWER_UNIT,
                 duration=duration,
             )
@@ -754,7 +754,7 @@ class FillRateBasedControlTUNES(FRBC):
         await self._fm_client.post_sensor_data(
             sensor_id=self._soc_maxima_sensor_id,
             start=fill_level_target_profile.start_time,
-            values=soc_maxima.values,
+            values=soc_maxima.tolist(),
             unit=POWER_UNIT,
             duration=duration,
         )
