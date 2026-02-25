3cee9a24e9c79746ff463c01c570d85de1fd0dfc
F.N. Claessen <claessen@seita.nl>
2026-02-25 17:42:07 +0100

    style: flake8
    Signed-off-by: F.N. Claessen <claessen@seita.nl>

--- tests/client/test_init.py
+++ tests/client/test_init.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import asyncio
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
 import pytest
 from aiohttp.client import ClientSession
